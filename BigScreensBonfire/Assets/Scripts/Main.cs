3dfd31dbcc4a8d14cf737be9edf2ca6770a2cd46
Julia Del Matto <jdelmatto@comcast.net>
2019-11-03 13:34:31 -0500

    Balls project stuff

---
+++ BigScreensBonfire/Assets/Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Main : MonoBehaviour
+{
+    private Connection _connection;
+    void Awake()
+    {
+        _connection = new
+Connection("https://bigscreens.herokuapp.com/socket.io/", "Balls",
+"game");
+        _connection.OnConnect(() =>
+        {
+            Debug.Log("CONNECTED");
+        });
+        _connection.OnDisconnect(() =>
+        {
+            Debug.Log("DISCONNECTED");
+        });
+        _connection.OnOtherConnect((id, type) =>
+        {
+            Debug.Log($"OTHER CONNECTED: {id} {type}");
+        });
+        _connection.OnOtherDisconnect((id, type) =>
+        {
+            Debug.Log($"OTHER DISCONNECTED: {id} {type}");
+        });
+        _connection.OnError((err) =>
+        {
+            Debug.LogError(err);
+        });
+        _connection.On("create-ball", (id) =>
+        {
+            Debug.Log($"CREATING BALL FOR {id}");
+            var ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            ball.transform.localScale = new Vector3(50, 50, 50);
+            ball.AddComponent<Rigidbody>
+().AddForce(Random.insideUnitCircle * 10000);
+            ball.AddComponent<SphereCollider>();
+        });
+        _connection.Open();
+    }
+}
