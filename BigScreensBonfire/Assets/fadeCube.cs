88b976031a0af0c00f1634f3d959e7c0cddad8ed
Julia Del Matto <jdelmatto@comcast.net>
2019-12-20 15:40:16 -0500

    Different fade key

--- BigScreensBonfire/Assets/fadeCube.cs
+++ BigScreensBonfire/Assets/fadeCube.cs
@@ -15,7 +15,7 @@ public class fadeCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.O))
         {
             cube.SetTrigger("FadeCube");
         }
