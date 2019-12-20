28ddd9ee0519f28aa4235a9c4fcd8cf794ed7d52
Julia Del Matto <jdelmatto@comcast.net>
2019-12-20 13:44:24 -0500

    final version, fixed flower positions

--- BigScreensBonfire/Assets/flowercontrol.cs
+++ BigScreensBonfire/Assets/flowercontrol.cs
@@ -27,6 +27,8 @@ public class flowercontrol : MonoBehaviour
     private bool flower4move = false;
 
     //
+
+        //
     // Start is called before the first frame update
     void Start()
     {
